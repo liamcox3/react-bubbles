330d53e4895c07f5caf06917f5a4d85b0c6db931
Liam Cox <liamcox@Liams-MacBook-Pro.local>
2020-06-19 11:07:55 -0500

    login

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,20 +1,32 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { axiosWithAuth } from "../utils/AxiosWithAuth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
-  const [colorList, setColorList] = useState([]);
-  // fetch your colors data from the server when the component mounts
-  // set that data to the colorList state property
+    const [colorList, setColorList] = useState([]);
+    // fetch your colors data from the server when the component mounts
+    // set that data to the colorList state property
+    useEffect(() => {
+        const token = window.localStorage.getItem("token");
+        axiosWithAuth()
+            .get("/api/colors")
+            .then((res) => {
+                setColorList(res.data);
+            })
+            .catch((error) => {
+                console.error("Server Error", error);
+            });
+    }, []);
 
-  return (
-    <>
-      <ColorList colors={colorList} updateColors={setColorList} />
-      <Bubbles colors={colorList} />
-    </>
-  );
+    return (
+        <>
+            <ColorList colors={colorList} updateColors={setColorList} />
+            <Bubbles colors={colorList} />
+        </>
+    );
 };
 
 export default BubblePage;
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,57 @@
 import React from "react";
+import { axiosWithAuth } from "../utils/AxiosWithAuth";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+class Login extends React.Component {
+    state = {
+        credentials: {
+            username: "",
+            password: "",
+        },
+    };
+
+    handleChange = (e) => {
+        this.setState({
+            credentials: {
+                ...this.state.credentials,
+                [e.target.name]: e.target.value,
+            },
+        });
+    };
+
+    login = (e) => {
+        e.preventDefault();
+        // Make a POST request and send the credentials object to the api
+        axiosWithAuth()
+            .post("/api/login", this.state.credentials)
+            .then((res) => {
+                localStorage.setItem("token", res.data.payload);
+                // navigate the user to /protected (whatever landing page)
+                this.props.history.push("/colors");
+            })
+            .catch((err) => console.log(err));
+    };
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.login}>
+                    <input
+                        type='text'
+                        name='username'
+                        value={this.state.credentials.username}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        type='password'
+                        name='password'
+                        value={this.state.credentials.password}
+                        onChange={this.handleChange}
+                    />
+                    <button>Log in</button>
+                </form>
+            </div>
+        );
+    }
+}
 
 export default Login;
